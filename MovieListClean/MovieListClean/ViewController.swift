659d5ffe846449ebc435211bcd289b7a3d311b4d
Pawee Kittiwathanakul <boatmind2710@gmail.com>
2019-09-19 21:25:41 +0700

    Install Project

---
+++ MovieListClean/MovieListClean/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieListClean
+//
+//  Created by Pawee Kittiwathanakul on 19/9/2562 BE.
+//  Copyright © 2562 Pawee Kittiwathanakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
